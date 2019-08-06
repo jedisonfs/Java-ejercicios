ee26c789751e50d69717d46424e66e501eea4f48
Jhon Edison Feria Sanabria <jeferia5@misena.edu.co>
2019-08-06 14:46:21 -0500

    chore: se agrego una variable booleana

--- ciclos/metodo/src/metodo/examen.java
+++ ciclos/metodo/src/metodo/examen.java
@@ -7,20 +7,25 @@ public class examen {
 	public void cedula() {
 		String[] cedu = { "1007385015", "1010032057", "10234562156", "1203458796", "1234561235" };
 		String ce="";
+		Boolean encontrado=false;
 			ce = JOptionPane.showInputDialog(null, "Ingrese su numero de cedula");
 		
 		for (int i = 0; i < cedu.length; i++) {
-			//do {
-		//		i++
-			
-			if (cedu[i].compareTo(ce)==0) {
+
+			if (cedu[i].equals(ce)) {
+				encontrado=true;
+				break;
+				}
+		}
+		if(encontrado==true) {		
 				JOptionPane.showMessageDialog(null,"Su cedula esta  registrada");
-				break;
-			}else  {
+				}
+		else {
 				JOptionPane.showMessageDialog(null,"Su numero de cedula no se encontro");
-				break;
+				
 			}
 		}
 		
 	}
-}
+
+
