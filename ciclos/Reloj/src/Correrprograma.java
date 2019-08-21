a2fdc0e3bff10fb9fc68ea142832ed5a71fbf550
Jhon Edison Feria Sanabria <jeferia5@misena.edu.co>
2019-08-21 17:43:42 -0500

    chore: agregando metodo menu

--- ciclos/Reloj/src/Correrprograma.java
+++ ciclos/Reloj/src/Correrprograma.java
@@ -2,24 +2,9 @@ import javax.swing.JOptionPane;
 
 public class Correrprograma {
 	public static void main(String[] args) {
-		Reloj1 ver = new Reloj1();
+		Reloj1 mirar = new Reloj1();
+		
+		mirar.ingresoDatos();
 
-		int opciones = Integer.parseInt(JOptionPane.showInputDialog(null,
-				"Digite la opcion que desea ver\n\n" + " 1. Horas minutos\n 2. Horas segundos\n 3. Minutos segundos"));
-
-		switch (opciones) {
-		case 1:
-			ver.ingresoDatos();
-			ver.conversionHora();
-			break;
-		case 2:
-			ver.ingresoDatos();
-			ver.conversionMinutos();
-			break;
-		case 3:
-			ver.ingresoDatos();
-			ver.conversionSegundos();
-			break;
-		}
 	}
 }
--- ciclos/Reloj/src/Reloj1.java
+++ ciclos/Reloj/src/Reloj1.java
@@ -1,11 +1,12 @@
 import javax.swing.JOptionPane;
 
 public class Reloj1 {
+	Reloj1 llamar = new Reloj1 ();
 	private int min = 0, hrs = 0, seg = 0;
 	private double gradoshr = 0, gradosmin = 0, gradosseg = 0, result = 0;
+	
 
 	public void ingresoDatos() {
-		JOptionPane.showMessageDialog(null, "Digite hora y luego minutos \n\n ");
 		do {
 			hrs = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite la hora "));
 			if (hrs < 1 || hrs > 12) {
@@ -48,6 +49,29 @@ public class Reloj1 {
 		result=gradosmin- gradosseg;
 		
 		JOptionPane.showMessageDialog(null, "Su hora en grados es: " +result);
+	}
+	public void menu() {
+		int llega = 0;
+
+		llamar.ingresoDatos();
+			int opciones = Integer.parseInt(JOptionPane.showInputDialog(null, "Digite la opcion que desea ver\n\n"
+					+ " 1. Horas minutos\n 2. Horas segundos\n 3. Minutos segundos\n 4 . Ingresar nueva hora \n 5 . Salir"));
+
+			switch (opciones) {
+			case 1:
+				llamar.conversionHora();
+				break;
+			case 2:
+				llamar.conversionMinutos();
+				break;
+			case 3:
+				llamar.conversionSegundos();
+				break;
+			case 4:
+				llamar.menu();break;
+			case 5:
+				System.exit(0);break;
+			}
 	}
 
 }
