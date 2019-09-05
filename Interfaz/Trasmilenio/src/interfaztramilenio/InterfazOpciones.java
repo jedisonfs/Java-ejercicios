ddbe3b2b719617f1311918437bcbb88c8e490e0d
-l <jeferia5@misena.edu.co>
2019-09-05 17:26:40 -0500

    chore: agregando botones

--- Interfaz/Trasmilenio/src/interfaztramilenio/InterfazOpciones.java
+++ Interfaz/Trasmilenio/src/interfaztramilenio/InterfazOpciones.java
@@ -10,7 +10,7 @@ import java.awt.event.*;
 
 public class InterfazOpciones extends JFrame implements ActionListener {
 	
-	private JButton boton1,boton2;
+	private JButton boton1,boton2,boton3;
 
 	public InterfazOpciones (){
 		
@@ -38,15 +38,28 @@ public class InterfazOpciones extends JFrame implements ActionListener {
 		
 		setLayout(null);
 		boton1 = new JButton ("Recargar tarjeta");
-		boton1.setBounds(100,100,90,30);
+		boton1.setBounds(80,130,150,30);
 		add(boton1);
 		boton1.addActionListener(this);
 		
+		boton2 = new JButton ("Ver saldo");
+		boton2.setBounds(80,300,150,30);
+        add(boton2);		
+        boton2.addActionListener(this);
+        
+        boton3 = new JButton("Personalizar tarjeta");
+        boton3.setBounds(3800,300,170,30);
+        add(boton3);
+        boton3.addActionListener(this);
 	}
 	public void actionPerformed (ActionEvent e) {
 		if(e.getSource()==boton1) {
 			
 		}
+	}
+	
+	public void recargaTarjeta() {
+		
 	}
 	
 	public static void main (String[] args) {
