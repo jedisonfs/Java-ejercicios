80f6a86f2b755195b9da6855ea05a69f21ab38c4
jedisonfs <jeferia5@misena.edu.co>
2020-03-10 23:46:43 -0500

    chore: se crea MySqlDAOFactory

--- PatronDAO/src/main/java/modelo/factory/mysqlfactory/MySqlTipoDocumentoDAOFactory.java
+++ PatronDAO/src/main/java/modelo/factory/mysqlfactory/MySqlTipoDocumentoDAOFactory.java
@@ -1,4 +1,12 @@
-package modelo.factory.MySqlTipoDocumentoDAOFactory;
+package modelo.factory.mysqlfactory;
 
-public class MySqlTipoDocumentoDAOFactory {
+import modelo.dao.TipoDocumentoDAO;
+import modelo.dao.mysqldao.MySqlTipoDocumentoDAO;
+import modelo.factory.TipoDocumentoDAOFactory;
+
+public class MySqlTipoDocumentoDAOFactory implements TipoDocumentoDAOFactory {
+    @Override
+    public TipoDocumentoDAO create() {
+        return new MySqlTipoDocumentoDAO();
+    }
 }
