65a36339edfd8c575873d5ff3e5ae9c331202bc7
jedisonfs <jeferia5@misena.edu.co>
2020-03-11 01:29:22 -0500

    chore: se termian test TipoDocumento

--- PatronDAO/src/test/java/modelo/dao/mysqldao/MySqlTipoDocumentoDAOTest.java
+++ PatronDAO/src/test/java/modelo/dao/mysqldao/MySqlTipoDocumentoDAOTest.java
@@ -1,5 +1,55 @@
+package modelo.dao.mysqldao;
+
+import modelo.dao.TipoDocumentoDAO;
+import modelo.dto.TipoDocumentoDTO;
+import modelo.factory.TipoDocumentoDAOFactory;
+import modelo.factory.mysqlfactory.MySqlTipoDocumentoDAOFactory;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MySqlTipoDocumentoDAOTest {
 
+    @Test
+    void insert() {
+        TipoDocumentoDTO tipoDocumentoDTO = new TipoDocumentoDTO();
+        tipoDocumentoDTO.setInitial("LM");
+        tipoDocumentoDTO.setDocumentName("Libreta militar");
+        tipoDocumentoDTO.setTypeDocumentState("1");
+
+        TipoDocumentoDAOFactory fabrica = new MySqlTipoDocumentoDAOFactory();
+        TipoDocumentoDAO daoTipoDocumento = fabrica.create();
+        assertEquals(daoTipoDocumento.insert(tipoDocumentoDTO),1);
+    }
+
+    @Test
+    void update() {
+        TipoDocumentoDTO tipoDocumentoDTO = new TipoDocumentoDTO();
+        tipoDocumentoDTO.setInitial("L.M");
+
+        TipoDocumentoDAOFactory fabrica = new MySqlTipoDocumentoDAOFactory();
+        TipoDocumentoDAO daoTipoDocumento = fabrica.create();
+        assertEquals(daoTipoDocumento.update(tipoDocumentoDTO,"18"),1);
+    }
+
+    @Test
+    void delete() {
+        TipoDocumentoDAOFactory fabrica = new MySqlTipoDocumentoDAOFactory();
+        TipoDocumentoDAO daoTipoDocumento = fabrica.create();
+        assertEquals(daoTipoDocumento.delete("18"),1);
+    }
+
+    @Test
+    void findByAll() {
+        TipoDocumentoDAOFactory fabrica = new MySqlTipoDocumentoDAOFactory();
+        TipoDocumentoDAO daoTipoDocumento = fabrica.create();
+        assertFalse(daoTipoDocumento.findByAll().isEmpty());
+    }
+
+    @Test
+    void findByPk() {
+        TipoDocumentoDAOFactory fabrica = new MySqlTipoDocumentoDAOFactory();
+        TipoDocumentoDAO daoTipoDocumento = fabrica.create();
+        assertNotEquals(daoTipoDocumento.findByPk("1"),null);
+    }
 }
