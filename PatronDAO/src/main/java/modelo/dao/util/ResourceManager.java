a24e73b3608b2a53e84bc5b16ef4e2089831942a
jedisonfs <jeferia5@misena.edu.co>
2020-03-10 23:36:40 -0500

    chore: se configura conexion

--- PatronDAO/src/main/java/modelo/dao/util/ResourceManager.java
+++ PatronDAO/src/main/java/modelo/dao/util/ResourceManager.java
@@ -1,4 +1,49 @@
 package modelo.dao.util;
 
+import java.sql.*;
+import java.util.logging.Logger;
+
 public class ResourceManager {
+
+    private static final String JDBC_URL = "jdbc:mysql://localhost:3306/ProyectoMio"+"?useTimezone=true&serverTimezone=UTF";
+    private static final String JDBC_USER = "root";
+    private static final String JDBC_PASSWORD = "123456789";
+
+    static final Logger logger = Logger.getLogger(ResourceManager.class.getName());
+
+    public static Connection getConexion(){
+        Connection conexion = null;
+
+        try {
+            conexion = DriverManager.getConnection(JDBC_URL, JDBC_USER, JDBC_PASSWORD);
+        }catch(SQLException e){
+            logger.info(e.getMessage());
+        }
+        return conexion;
+    }
+
+    public static void closeConexion(Connection conexion){
+        try{
+            conexion.close();
+        }catch(SQLException e){
+            logger.info(e.getMessage());
+        }
+    }
+
+    public static void closeSentencia(PreparedStatement senten){
+        try{
+            senten.close();
+        }catch(SQLException e){
+            logger.info(e.getMessage());
+        }
+    }
+
+    public static void closeResultado(ResultSet resul){
+        try{
+            resul.close();
+        }catch(SQLException e){
+            logger.info(e.getMessage());
+        }
+    }
 }
+
