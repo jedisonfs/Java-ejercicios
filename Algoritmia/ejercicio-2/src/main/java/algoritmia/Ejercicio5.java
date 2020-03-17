eb904653783b2d0fd961c30071e4ebf832bc9dc1
jedisonfs <jeferia5@misena.edu.co>
2020-03-16 19:31:13 -0500

    chore: se termina Ejercicio 5

--- Algoritmia/ejercicio-2/src/main/java/algoritmia/Ejercicio5.java
+++ Algoritmia/ejercicio-2/src/main/java/algoritmia/Ejercicio5.java
@@ -2,7 +2,7 @@ package algoritmia;
 
 import java.util.Scanner;
 
-public class Ejercico5 {
+public class Ejercicio5 {
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
