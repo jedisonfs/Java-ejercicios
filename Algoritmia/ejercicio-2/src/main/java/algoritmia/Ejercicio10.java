4aaa783385b5dbf04f68a9271db439bfb116f6b7
jedisonfs <jeferia5@misena.edu.co>
2020-03-18 12:50:01 -0500

    chore: se termina Ejercicio 10

--- Algoritmia/ejercicio-2/src/main/java/algoritmia/Ejercicio10.java
+++ Algoritmia/ejercicio-2/src/main/java/algoritmia/Ejercicio10.java
@@ -1,4 +1,28 @@
 package algoritmia;
 
+import java.util.Scanner;
+
 public class Ejercicio10 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n1 = 0;
+        int a = 0;
+        int b = 0;
+
+
+        System.out.println("Digite un numero ");
+        n1 = sc.nextInt();
+
+        a = n1/10;
+        b = n1%10;
+
+        if (a == b ){
+            System.out.println(a+" es igual  "+b);
+        }else{
+            System.out.println("Son diferentes");
+        }
+
+    }
 }
