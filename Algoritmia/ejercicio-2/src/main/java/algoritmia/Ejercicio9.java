b584045ede7fe0feacb0f9d7a7d87e52ccc6f848
jedisonfs <jeferia5@misena.edu.co>
2020-03-18 12:49:48 -0500

    chore: se termina Ejercicio 9

--- Algoritmia/ejercicio-2/src/main/java/algoritmia/Ejercicio9.java
+++ Algoritmia/ejercicio-2/src/main/java/algoritmia/Ejercicio9.java
@@ -1,4 +1,26 @@
 package algoritmia;
 
+import java.util.Scanner;
+
 public class Ejercicio9 {
+    public static void main(String[]args){
+
+        Scanner sc = new Scanner(System.in);
+
+        int n1 = 0;
+        int a = 0;
+        int b = 0;
+
+        System.out.println("Digite un numero ");
+        n1 = sc.nextInt();
+
+        a = n1/10;
+        b = n1%10;
+
+        if (a%b == 0 ){
+            System.out.println(a+" es multiplo de "+b);
+        }else{
+            System.out.println("No son multiplos");
+        }
+    }
 }
