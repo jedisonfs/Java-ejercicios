6df1a2d612bf57bd3e6c249ec8174d5e159f2333
jedisonfs <jeferia5@misena.edu.co>
2020-03-18 12:50:44 -0500

    chore: se termina Ejercicio 13

--- Algoritmia/ejercicio-2/src/main/java/algoritmia/Ejercicio13.java
+++ Algoritmia/ejercicio-2/src/main/java/algoritmia/Ejercicio13.java
@@ -1,4 +1,28 @@
 package algoritmia;
 
+import java.util.Scanner;
+
 public class Ejercicio13 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n1 = 0;
+        int n2 = 0;
+        int suma = 0;
+
+        System.out.println("Digite numero uno");
+        n1 = sc.nextInt();
+
+        System.out.println("Digite un segundo numero ");
+        n2 = sc.nextInt();
+
+        suma = n1+n2;
+
+        if (suma % 2 == 0){
+            System.out.println("La suma es numero par "+suma);
+        }else{
+            System.out.println("No es par "+suma);
+        }
+    }
 }
