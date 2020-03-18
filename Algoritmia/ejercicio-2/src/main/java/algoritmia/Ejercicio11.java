6a28206043759c8ad2c9c91dd1ec1e384054e8a4
jedisonfs <jeferia5@misena.edu.co>
2020-03-18 12:50:09 -0500

    chore: se termina Ejercicio 11

--- Algoritmia/ejercicio-2/src/main/java/algoritmia/Ejercicio11.java
+++ Algoritmia/ejercicio-2/src/main/java/algoritmia/Ejercicio11.java
@@ -1,4 +1,27 @@
 package algoritmia;
 
+import java.util.Scanner;
+
 public class Ejercicio11 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n1 = 0;
+        int a = 0;
+        int b = 0;
+
+
+        System.out.println("Digite un numero ");
+        n1 = sc.nextInt();
+
+        a = n1/10;
+        b = n1%10;
+
+        if (a > b ){
+            System.out.println(a+" es mayor que "+b);
+        }else if (b > a){
+            System.out.println(b+ " es mayor que ");
+        }
+    }
 }
