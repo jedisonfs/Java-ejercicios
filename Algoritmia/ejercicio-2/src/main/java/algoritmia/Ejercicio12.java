f4304d2f08261ba59c3c89108d287e735c17743f
jedisonfs <jeferia5@misena.edu.co>
2020-03-18 12:50:37 -0500

    chore: se termina Ejercicio 12

--- Algoritmia/ejercicio-2/src/main/java/algoritmia/Ejercicio12.java
+++ Algoritmia/ejercicio-2/src/main/java/algoritmia/Ejercicio12.java
@@ -1,4 +1,35 @@
 package algoritmia;
 
+import java.util.Scanner;
+
 public class Ejercicio12 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int num1;
+        int a = 0;
+        int b = 0;
+        int cont = 0;
+        int cont2 = 0;
+
+        System.out.println("Digite un numero");
+        num1 = sc.nextInt();
+
+        a = num1/10;
+        b = num1%10;
+
+        for(int i=1;i < 20;i++){
+            if(a % i == 0){
+                cont++;
+            }if(b % i == 0){
+                cont2++;
+            }
+        }
+
+        if(cont == cont2){
+            System.out.println("Tiene numeros iguales");
+        }else{
+            System.out.println("no tienen numeros iguales ");
+        }
+    }
 }
