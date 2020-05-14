07b5a4aad9b97628ca3b9005302ed6ca599f6135
jedisonfs <jeferia5@misena.edu.co>
2020-05-14 00:44:12 -0500

    chore: se agregan types dates

---
+++ javascript/js/typeDate.js
@@ -0,0 +1,16 @@
+"use script"
+
+// boolean
+var numero = 100;
+//String
+var string = "string";
+//Boolean
+var boolean = true;
+// fechas
+var fecha = new Date();
+//Symbol
+var pi = Symbol(3.1416);
+
+fecha = 07 / 05 / 2001;
+
+console.log(pi);
