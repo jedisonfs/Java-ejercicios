8efc6423b57dcd3c6466652314dd7ee0e01a113c
jedisonfs <jeferia5@misena.edu.co>
2020-05-14 00:43:48 -0500

    chore: se agregan type variables

---
+++ javascript/js/typeVariables.js
@@ -0,0 +1,17 @@
+"use script"
+
+// Variable local
+let num1 = 123;
+// Variable global
+var name = "1234";
+// Constante
+const constante = 3.1416;
+
+console.log(num1);
+console.log(name);
+console.log(constante);
+
+// Se esta pasando la variable name en un Number
+let numeroNuevo = Number(name);
+
+console.log(numeroNuevo);
