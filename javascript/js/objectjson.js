cc85515726ee71e901b8e6ead77ce0dcd69ba5fd
jedisonfs <jeferia5@misena.edu.co>
2020-05-14 00:43:15 -0500

    chore: se agregan Object JSON

---
+++ javascript/js/objectjson.js
@@ -0,0 +1,16 @@
+"use script"
+
+var persona = {
+    name: "Sergio",
+    edad: 17
+};
+
+var personas = [{
+        name: "edison",
+        edad: 18
+    },
+    {
+        name: "Camila",
+        edad: 21
+    }
+]
