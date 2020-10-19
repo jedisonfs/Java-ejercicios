f072c46e6cc44572cbf34b3c1f91f733473b05e7
Edison Feria <edisonferia4@gmail.com>
2020-10-19 13:42:47 -0500

    chore: se agrega el projecto JPA

---
+++ curso-spring/Jpa2/src/test/java/co/edu/sena/testsring/web/Jpa2ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.sena.testsring.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jpa2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
