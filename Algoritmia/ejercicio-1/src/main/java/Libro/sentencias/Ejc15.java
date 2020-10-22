c4fed7710682cd19ffaf12e4a602c37e50cde81a
Edison Feria <edisonferia4@gmail.com>
2020-10-22 11:34:44 -0500

    chore: se agrega Ejc114 l 16

---
+++ Algoritmia/ejercicio-1/src/main/java/Libro/sentencias/Ejc15.java
@@ -0,0 +1,15 @@
+package Libro.sentencias;
+
+public class Ejc15 {
+
+	public static void main(String [] args) {
+		
+		int n1=0;
+		int a=n1/100;
+		int b=a/10;
+		int c=a%10;
+		
+		int suma = a+b+c;
+		System.out.println("La suma es de "+suma);
+	}
+}
---
+++ Algoritmia/ejercicio-1/src/main/java/Libro/sentencias/Ejc16.java
@@ -0,0 +1,9 @@
+package Libro.sentencias;
+
+public class Ejc16 {
+
+	public static void main(String [] args) {
+		
+		
+	}
+}
