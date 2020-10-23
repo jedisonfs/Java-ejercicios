424e3e4893e9a0315fe1218e9b57064f23197241
Edison Feria <edisonferia4@gmail.com>
2020-10-23 11:56:23 -0500

    chore: se agregan Ejc20 a 21

---
+++ Algoritmia/ejercicio-1/src/main/java/Libro/sentencias/EEjc21.java
@@ -0,0 +1,8 @@
+package Libro.sentencias;
+
+public class EEjc21 {
+
+	public static void main(String [] args) {
+		
+	}
+}
--- Algoritmia/ejercicio-1/src/main/java/Libro/sentencias/Ejc20.java
+++ Algoritmia/ejercicio-1/src/main/java/Libro/sentencias/Ejc20.java
@@ -2,12 +2,26 @@ package Libro.sentencias;
 
 public class Ejc20 {
 
-	public static void main(String [] args) {
-		
-		int n1=0;
-		int n2=0;
-		int n3=0;
-	
-		
+	public static void main(String[] args) {
+
+		int n1 = 1;
+		int n2 = 5;
+		int n3 = 2;
+
+		if (n1 > n2 && n1 > n3 && n2 > n3) {
+			System.out.println(n1 + n2 + n3);
+		} else if (n1 > n2 && n1 > n3 && n3 > n2) {
+			System.out.println(n1 + n3 + n2);
+		} else if (n2 > n1 && n2 > n3 && n1 < n3) {
+			System.out.println(n2 + n1 + n3);
+		} else if (n2 > n1 && n2 > n3 && n3 > n1) {
+			System.out.println(n2 + n3 + n1);
+		} else if (n3 > n2 && n3 > n1 && n2 > n1) {
+			System.out.println(n3 + n2 + n1);
+		} else if (n3 > n2 && n3 > n1 && n1 > n2) {
+			System.out.println(n3 + n1 + n2);
+		}else {
+			System.out.println("Error en los numeros ingrsados");
+		}
 	}
 }
