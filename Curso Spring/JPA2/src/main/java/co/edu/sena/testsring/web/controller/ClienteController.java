a6ecd458bb8a98f798f8d2c674a553b6c21ff5fb
Edison Feria <edisonferia4@gmail.com>
2020-10-23 14:18:44 -0500

    chore: se agrega funcion de subir archivos endpoint Save

--- Curso Spring/JPA2/src/main/java/co/edu/sena/testsring/web/controller/ClienteController.java
+++ Curso Spring/JPA2/src/main/java/co/edu/sena/testsring/web/controller/ClienteController.java
@@ -1,11 +1,15 @@
 package co.edu.sena.testsring.web.controller;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Map;
 import java.util.Optional;
 import java.util.logging.Logger;
 
 import javax.validation.Valid;
 
+import org.apache.tomcat.jni.File;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -27,6 +31,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import co.edu.sena.testsring.web.models.dao.repository.ClienteDao;
 import co.edu.sena.testsring.web.models.entity.Cliente;
@@ -47,17 +53,30 @@ public class ClienteController {
 	}
 
 	@RequestMapping(value = "/listar", method = RequestMethod.GET)
-	public String listar(@RequestParam(name = "page", defaultValue = "0") int page , Model model) {
-		
+	public String listar(@RequestParam(name = "page", defaultValue = "0") int page, Model model) {
+
 		Pageable pageableRequest = PageRequest.of(page, 5);
-		
-		Page<Cliente> clientes =  clienteService.findAll(pageableRequest);
-		
+
+		Page<Cliente> clientes = clienteService.findAll(pageableRequest);
+
 		PaginatorReder pageRender = new PaginatorReder("/listar", clientes);
 		model.addAttribute("titulo", "Listado de clientes");
 		model.addAttribute("cliente", clientes);
 		model.addAttribute("pager", pageRender);
 		return "listar";
+	}
+
+	@GetMapping("/ver/{id}")
+	public String ver(@PathVariable(value = "id") Long id, RedirectAttributes flash, Map<String, Object> model) {
+		Optional<Cliente> cliente = clienteService.findById(id);
+		if (cliente == null) {
+			flash.addAttribute("error", "El cliente no esta registrado en la base de datos");
+			return "redirect:/listar";
+		}
+
+		model.put("cliente", cliente);
+		model.put("titulo", "Datos del cliente");
+		return "ver";
 	}
 
 	@RequestMapping("/form")
@@ -82,10 +101,27 @@ public class ClienteController {
 
 	// @RequestMapping(value = "/form", method = RequestMethod.POST)
 	@PostMapping("/form")
-	public String save(@Valid Cliente cliente, BindingResult result, Model model, SessionStatus status) {
+	public String save(@Valid Cliente cliente, BindingResult result, Model model, SessionStatus status,
+			@RequestParam("file") MultipartFile foto, RedirectAttributes flash) {
 		if (result.hasErrors()) {
 			model.addAttribute("titulo", "Formulario de cliente");
 			return "form";
+		}
+
+		if (!foto.isEmpty()) {
+			Path direcionRecursos = Paths.get("src/main/resources/static/upload/");
+			String rootPath = direcionRecursos.toFile().getAbsolutePath();
+
+			try {
+				byte[] bytes = foto.getBytes();
+
+				Path rutaCompleta = Paths.get(rootPath + "/" + foto.getOriginalFilename());
+				Files.write(rutaCompleta, bytes);
+				flash.addAttribute("info", "Has subido correctamente el archivo");
+				cliente.setFoto(foto.getOriginalFilename());
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 		clienteService.save(cliente);
 		status.setComplete();
