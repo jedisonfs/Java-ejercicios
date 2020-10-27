66c11f05f2401610de404493c72a27107d86b242
Edison Feria <edisonferia4@gmail.com>
2020-10-27 18:44:15 -0500

    chore: se agrega Ejc21

--- Algoritmia/ejercicio-1/src/main/java/Libro/sentencias/EEjc21.java
+++ Algoritmia/ejercicio-1/src/main/java/Libro/sentencias/EEjc21.java
@@ -2,7 +2,14 @@ package Libro.sentencias;
 
 public class EEjc21 {
 
-	public static void main(String [] args) {
+	public static void main(String[] args) {
+
+		int n1 = 24;
+		int n2 = 0;
+		int a = n1 / 10;
+		int b = n1 % 10;
+		int c = n2 / 10;
+		int d = n2 % 10;
 		
 	}
 }
